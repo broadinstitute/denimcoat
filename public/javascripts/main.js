bf4a3c23ddb1bee3032a106978a506303fa172b4
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-05-07 12:02:55 -0400

    Adding timers for debugging.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,11 @@
+printTime(d3.select("#loadTime"));
+setInterval(function () {
+    printTime(d3.select("#nowTime"))
+}, 200);
+
+function printTime(element) {
+    var nowTime = new Date();
+    element.html(nowTime.toLocaleTimeString());
+}
+
 d3.select("#greeting").style("color", "red");
