4a373e82713401680698e51717fd8995cfcf4c79
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-05-07 15:31:14 -0400

    Buttons now are responsive (earlier attempts failed due to security policy violations).

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,4 +8,19 @@ function printTime(element) {
     element.html(nowTime.toLocaleTimeString());
 }
 
-d3.select("#greeting").style("color", "red");
+d3.select("#inputSubmitButton").on("click", submitQuestion);
+d3.select("#inputExampleButton").on("click", setExample);
+d3.select("#inputClearButton").on("click", clearInput);
+
+function submitQuestion() {
+    alert(d3.select("#input").property("value"));
+}
+
+var exampleInput = "How many roads must a man walk down, before you call him a man?";
+
+function setExample() {
+    d3.select("#input").property("value", exampleInput);
+}
+function clearInput() {
+    d3.select("#input").property("value", "");
+}
