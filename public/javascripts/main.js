a42ca65c175d806136ee4f0462568331d30c3814
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-05-22 16:31:57 -0400

    Adding Cytoscape.js 2.3.12.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -221,3 +221,40 @@ function setExample() {
 function clearInput() {
     d3.select("#input").property("value", "");
 }
+
+const cy = cytoscape({
+    container: d3.select("#cytoscape").node(),
+    elements: [ // list of graph elements to start with
+        { // node a
+            data: { id: 'a' }
+        },
+        { // node b
+            data: { id: 'b' }
+        },
+        { // edge ab
+            data: { id: 'ab', source: 'a', target: 'b' }
+        }
+    ],
+    style: [ // the stylesheet for the graph
+        {
+            selector: 'node',
+            style: {
+                'background-color': '#666',
+                'label': 'data(id)'
+            }
+        },
+        {
+            selector: 'edge',
+            style: {
+                'width': 3,
+                'line-color': '#ccc',
+                'target-arrow-color': '#ccc',
+                'target-arrow-shape': 'triangle'
+            }
+        }
+    ],
+    layout: {
+        name: 'grid',
+        rows: 1
+    }
+});
