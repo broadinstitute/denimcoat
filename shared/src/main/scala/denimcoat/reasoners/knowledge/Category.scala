489823f3748db71627aa7ff5aff38e35f1180a17
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-09-10 10:53:47 -0400

    Switch phenotype key from "hp" to "HP" to make Monarch Initiative work.

--- shared/src/main/scala/denimcoat/reasoners/knowledge/Category.scala
+++ shared/src/main/scala/denimcoat/reasoners/knowledge/Category.scala
@@ -9,7 +9,7 @@ object Category {
   def apply(name: String, prefix: String): Category = Category(name, name, prefix)
 
   val disease = Category("disease", "omim.disease")
-  val symptom = Category("symptom", "hp")
+  val symptom = Category("symptom", "HP")
   val gene = Category("gene", "ncbigene")
   val variant = Category("variant", "rs")
 }
