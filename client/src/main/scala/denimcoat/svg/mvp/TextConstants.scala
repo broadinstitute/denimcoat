c2d7d5bf2ff05564f4f678d77f7dbfde49bc6ee8
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-09-13 12:25:03 -0400

    Minor. Plus changing "ex" to "e.g.".

---
+++ client/src/main/scala/denimcoat/svg/mvp/TextConstants.scala
@@ -0,0 +1,10 @@
+package denimcoat.svg.mvp
+
+object TextConstants {
+
+  val exampleButton: String = "e.g."
+  val clearButton: String = "clear"
+
+  val reasonerSelectionCheckMark: String = "X"
+
+}
